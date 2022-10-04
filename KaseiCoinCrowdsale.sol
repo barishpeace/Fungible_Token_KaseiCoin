99e08d4d14ed4a65eef9089e37f24540a361bab4
Barish Peace <99712543+barishpeace@users.noreply.github.com>
2022-10-04 13:02:58 -0700

    Update KaseiCoinCrowdsale.sol

--- KaseiCoinCrowdsale.sol
+++ KaseiCoinCrowdsale.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.5.0;
 import "./KaseiCoin.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
 
 
 // Bootstrap the KaseiCoinCrowdsale contract by inheriting the following OpenZeppelin:
